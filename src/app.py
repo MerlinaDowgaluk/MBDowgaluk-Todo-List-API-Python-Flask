08947fda5590d47f062c149a15ba5451258cdb53
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-12-12 19:14:04 +0000

    add get with position, and conditional

--- src/app.py
+++ src/app.py
@@ -1,28 +1,30 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/todos', methods=['GET'])
-def hello():
-    json_text = jsonify(todos)
-    # response_body = todos -> al ser list/dict lo interpreta como json directamente
-    return json_text
-
-@app.route('/todos', methods=['POST'])
-def add_new_todo():
-    request_body = request.json
-    print("Incoming request with the following body", request_body)
-    todos.append(request_body)
-    response_body = todos
-    return response_body
-
-@app.route('/todos/<int:position>', methods=['DELETE'])
-def delete_todo(position):
-    print("This is the position to delete: ",position)
-    del todos[position]
-    response_body = todos
-    return response_body
+@app.route('/todos', methods=['GET', 'POST'])
+def handle_todos():
+    if request.method == 'GET':
+        response_body = todos
+        return response_body
+    if request.method == 'POST':
+        request_body = request.json
+        print("Incoming request with the following body", request_body)
+        todos.append(request_body)
+        response_body = todos
+        return response_body
 
 
+@app.route('/todos/<int:position>', methods=['GET','DELETE'])
+def handle_todos_position(position):
+    if request.method == 'GET':
+        response_body = todos[position]
+        # response_body = todos -> al ser list/dict lo interpreta como json directamente
+        return response_body
+    if request.method == 'DELETE':
+        print("This is the position to delete: ",position)
+        del todos[position]
+        response_body = todos
+        return response_body
 
 todos = [
     { "label": "My first task", "done": False },
